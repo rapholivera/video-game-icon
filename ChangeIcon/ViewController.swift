56824c193b13a0b623b2c481b6c43096c2c69058
Raphael Oliveira <raphzouz@gmail.com>
2020-10-10 16:13:17 -0300

    Initial Commit

---
+++ ChangeIcon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeIcon
+//
+//  Created by Raphael Oliveira on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
